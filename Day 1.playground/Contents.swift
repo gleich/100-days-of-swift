bc90cf47897651d8bcbe9257ac246415b808ec88
Matt Gleich <git@mattglei.ch>
2023-06-26 17:12:02 -0400

    feat: day 1
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+// strings and integers
+var str = "Hello, playground"
+var age = 38
+var population = 8_000_000 // same as 8000000
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// doubles and booleans
+var pi = 3.141 // double
+var piRounded = 3 // integer
+var awesome = true // boolean
+
+// string interpolation
+var score = 85
+var scoreStr = "Your score was \(score)"
+print(scoreStr)
+
+// constants
+let taylor = "swift" // this cannot be changed
+
+// type annotations
+let string = "Hello, playground" // type inteference for String
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
